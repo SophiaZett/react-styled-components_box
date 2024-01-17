8f0cf5e510a959cfc2ad7e6468939a6d43c27fee
Sophia S. <sophia0791@icloud.com>
2024-01-17 16:38:34 +0100

    solve tasks

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 import BoxWithClassName from "../components/BoxWithClassName/BoxWithClassName.js";
+import StyledBox from "../components/BoxWithStyledComponents/BoxWithStyledComponents.js";
 
 export default function HomePage() {
   return (
-    <div>
-      <BoxWithClassName />
-      <BoxWithClassName isBlack />
+    <div style={{ display: "flex" }}>
+      <BoxWithClassName isBlack={false} />
+      <BoxWithClassName isBlack={true} />
+      <StyledBox $isBlack={false} />
+      <StyledBox $isBlack={true} />
     </div>
   );
 }
